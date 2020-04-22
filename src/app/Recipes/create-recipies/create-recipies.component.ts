ea33869373ce411b88231dcdf5c9a785688780e1
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-04-22 13:25:08 +0100

    set up anggular project and created a component

---
+++ src/app/Recipes/create-recipies/create-recipies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-recipies',
+  templateUrl: './create-recipies.component.html',
+  styleUrls: ['./create-recipies.component.scss']
+})
+export class CreateRecipiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRecipiesComponent } from './Recipes/create-recipies/create-recipies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRecipiesComponent
   ],
   imports: [
     BrowserModule,
